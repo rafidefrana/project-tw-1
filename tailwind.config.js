9ec72355706ead4b549300f93303ed74f7542062
rafidefrana <rafidefrana08@gmail.com>
2024-01-30 07:50:14 +0700

    fixed bug deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
   theme: {
     screens: {
       sm: '480px',
